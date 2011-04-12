f6acb0b6dc4c31649ca8f8e741628315db79e103
Manish Kumar <mkr@openlabs.co.in>
2011-04-12 20:13:53 +0530

    initial release

--- build/lib.linux-x86_64-2.6/trytond/modules/nereid_shipping/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: UTF-8 -*-
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
-"Nereid Shipping"
-
-from .shipping import *
-from .methods import *
--- build/lib.linux-x86_64-2.6/trytond/modules/nereid_shipping/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-    callisto_gateway.tests
-
-    Tests Module
-
-    :copyright: (c) 2010-2011 by Openlabs Technologies & Consulting (P) Ltd.
-    :license: GPLv3, see LICENSE for more details
-'''
-import test_shipping
